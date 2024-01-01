6c4122a22bb92b5918048e5ee94e2f7d6257e090
ushi <>
2024-01-01 17:44:25 +0900

    feat: riverpod仕様に変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kurumo/config/router_provider.dart';
 import 'package:kurumo/feature/auth/widget/login_page.dart';
 import 'package:kurumo/firebase_options.dart';
 
@@ -8,18 +10,18 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      routerConfig: ref.watch(goRouterProvider),
       theme: ThemeData(),
-      home: const LoginPage(),
     );
   }
 }
