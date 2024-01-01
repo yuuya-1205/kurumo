3ca4384de46a2d34166f92c54bbe4efb086d0bd4
ushi <>
2024-01-01 17:44:36 +0900

    feat: routeの設定

---
+++ lib/config/router_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:kurumo/feature/auth/widget/login_page.dart';
+import 'package:kurumo/feature/auth/widget/selected_page.dart';
+import 'package:riverpod/riverpod.dart';
+
+final goRouterProvider = Provider(
+  (ref) {
+    return GoRouter(
+      ///最初のページってこと？
+
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const LoginPage();
+          },
+        ),
+        GoRoute(
+          path: '/selected_page',
+          builder: (context, state) {
+            return const SelectedPage();
+          },
+        ),
+      ],
+    );
+  },
+);
